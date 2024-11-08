3c4101e8921c50985803e59125076b0ff5a9632c
Sriparna Koar <koarsk03@gmail.com>
2024-11-08 09:43:03 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@
 // }
 module.exports = {
   env: {
-    NEXTAUTH_URL: 'http://localhost:3000'
+    // NEXTAUTH_URL: 'http://localhost:3000'
+      NEXTAUTH_URL: process.env.NEXTAUTH_URL || 'http://localhost:3000'
   }
 };
